047857a5fcd0bec619b90e1d4b6b1b3ef262b175
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2024-08-02 17:40:04 +0400

    upd css for body and for project card

--- src/components/UI/project_container/ProjectContainer.jsx
+++ src/components/UI/project_container/ProjectContainer.jsx
@@ -3,27 +3,52 @@ import classes from "./ProjectContainer.module.css"
 import ProjectCard from "../project_card/ProjectCard";
 
 const ProjectContainer = (props) => {
+
+    let view;
+
+    if (props.project.view < 10**3) {
+        view = props.project.view;
+    } else if (100 > props.project.view / 10**3 > 0){
+        view = `${Math.round(props.project.view / 10**3)} тыс`;
+    } else if (props.project.view / 10**6 > 0){
+        view = `${Math.round(props.project.view / 10**6)} млн`;
+    }
     return (
         <div className={classes.container}>
-            <button className={classes.btn}>
+            <div className={classes.wrapperInfoCard}>
                 <ProjectCard project={props.project}/>
                 <div className={classes.infoCard}>
                     <div className={classes.wrapperInfoText}>
-                        <p className={classes.infoText}>
-                            Название: {props.project.title}
+                        <p className={classes.infoTextName}>
+                            {props.project.title}
                         </p>
                     </div>
                     <div className={classes.wrapperInfoText}>
                         <p className={classes.infoText}>
-                            Описание: {props.project.desc.length >= 50
-                            ? props.project.desc.slice(0, 100) + '...'
-                            : props.project.desc}
+                            Просмотры: {view}
+                        </p>
+                        <p className={classes.infoText}>
+                            Рейтинг: {props.project.rate}/10
+                        </p>
+                        <p className={classes.infoText}>
+                            <div className={classes.wrapperStatusCode}>
+                                Статус:
+                                {props.project.status === 'active'
+                                    ? <p className={classes.active}>работает</p>
+                                    : props.project.status === 'in develop'
+                                        ? <p className={classes.develop}>в разработке</p>
+                                        : <p className={classes.inactive}>не работает</p>
+                                }
+                            </div>
                         </p>
                     </div>
+                    <button className={classes.moreInfoBtn}>
+                        <h1 className={classes.moreInfoText}>Подробнее</h1>
+                    </button>
                 </div>
-            </button>
+            </div>
         </div>
-);
+    );
 };
 
 export default ProjectContainer;
