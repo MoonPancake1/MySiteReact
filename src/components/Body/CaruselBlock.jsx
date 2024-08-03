c38efd99d5fab2594aec48e21228e23cf39db88f
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2024-08-03 15:18:45 +0400

    HotFix: replace img in carusel card

--- src/components/Body/CaruselBlock.jsx
+++ src/components/Body/CaruselBlock.jsx
@@ -9,26 +9,26 @@ const CaruselBlock = () => {
         {
             'title': 'vC.ID',
             'description': 'vC.ID – система авторизации',
-            'image': '',
+            'image': 'https://i.pinimg.com/564x/2b/c7/77/2bc7771c0457d542cea5a7574e75c395.jpg',
         },
         {
             'title': 'vC.ID',
             'description': 'vC.ID – система авторизации',
-            'image': '',
+            'image': 'https://i.pinimg.com/564x/07/21/1b/07211b078ab5e9f537a3daeccef72279.jpg',
         },
         {
             'title': 'vC.ID',
             'description': 'vC.ID – система авторизации',
-            'image': '',
+            'image': 'https://i.pinimg.com/564x/d4/42/9e/d4429e833937f4733fa37d8e86b3675f.jpg',
         }
     ]
 
-    const images = require.context('../../images/projectImages', true);
-    const imageList = images.keys().map((image) => images(image));
-
-    for (let i = 0; i < imagesData.length; i++){
-        imagesData[i].image = imageList[i]
-    }
+    // const images = require.context('../../images/projectImages', true);
+    // const imageList = images.keys().map((image) => images(image));
+    //
+    // for (let i = 0; i < imagesData.length; i++){
+    //     imagesData[i].image = imageList[i]
+    // }
 
     return (
         <div className={classes.container}>
